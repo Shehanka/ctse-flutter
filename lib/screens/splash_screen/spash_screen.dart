6b49bf3eb030a21f22a3eda58b847ce107a9892d
Shehanka <hcsperera@outlook.com>
2021-06-13 22:37:43 +0530

    iOS Firebase setup

--- lib/screens/splash_screen/spash_screen.dart
+++ lib/screens/splash_screen/spash_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ctse_flutter/services/auth/authentication.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -6,10 +8,59 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  BaseAuthentication _authentication;
+  User _firebaseUser;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _authentication = Authentication();
+    _firebaseUser = FirebaseAuth.instance.currentUser;
+
+    _authentication.getCurrentUser();
+  }
+
   @override
   Widget build(BuildContext context) {
     throw Scaffold(
-      body: Text("hello"),
-    );
+        body: Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.deepPurpleAccent,
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.deepPurple,
+              radius: 65.0,
+              child: Icon(
+                Icons.school,
+                color: Colors.white70,
+                size: 70.0,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0),
+            ),
+            Text(
+              'Fantastic Notes',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40.0,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 70.0),
+            ),
+            CircularProgressIndicator(),
+          ],
+        )
+      ],
+    ));
   }
 }
